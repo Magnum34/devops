f672de1312e14a9302943d793d968475b4faacda
Magnum34 <mariusz@qunabu.com>
2020-01-04 03:06:46 +0100

    add laravel, wordpress, silverstripe4

---
+++ hosting/webfaction/README.md
@@ -0,0 +1,3 @@
+# Webfactional
+
+## Usage
---
+++ hosting/webfaction/roles/wordpress/templates/wp-config.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', '{{database_name}}');
+/** MySQL database username */
+define( 'DB_USER', '{{database_name}}');
+/** MySQL database password */
+define( 'DB_PASSWORD', '{{database_password}}');
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost');
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+{{ wp_salt.stdout }}
+/**#@-*/
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+/* That's all, stop editing! Happy publishing. */
+/* That's all, stop editing! Happy blogging. */
+if (!defined('WP_CONTENT_DIR')) {
+    define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
+}
+if (!defined('WP_CONTENT_URL')) {
+    define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+}
+if (!defined('WP_SITEURL')) {
+    define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/');
+}
+if (!defined('WP_HOME')) {
+    define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . '');
+}
+if ( !defined( 'ABSPATH' ) )
+    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+require_once( ABSPATH . 'wp-settings.php' );
